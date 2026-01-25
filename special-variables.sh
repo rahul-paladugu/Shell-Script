d3ac95dee4e43bc340153539476be5a86e4dd9d7
rahul paladugu <rahulpaladugu96@gmail.com>
2026-01-26 10:55:41 +1300

    shell-variables

--- special-variables.sh
+++ special-variables.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+echo "All variables passed to this script are $@"
+echo "All variables passed to this script are $*"
+echo "current working directory is $pwd"
+echo "who is running this script: $user"
+echo "PID of the script is $$"
+sleep 10 &
+echo "PID of last command in background is $!"
