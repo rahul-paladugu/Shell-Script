ed25f0c0a45a2e5ed41d386d49e32f97f9c00bdb
rahul paladugu <rahulpaladugu96@gmail.com>
2026-01-26 12:08:55 +1300

    conditions

---
+++ conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [$env = PROD]; then
+   echo "PROD environment is selected. So right sizing is "t3.large""
+elif [$env = UAT]; then
+   echo "UAT environment is selected. So right sizing is "t3.medium""
+elif [$env = DEV]; then
+   echo "DEV environment is selected. So right sizing is "t3.small""
+else
+   echo "You have selected incorrect environment, please select PROD or UAT or DEV"
+fi
