549ec4b1d5a33c115c62cb5a85e8402e0acd1e41
rahul paladugu <rahulpaladugu96@gmail.com>
2026-01-26 10:45:34 +1300

    Shell-Variables

---
+++ Dynamic-varibales.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# output of commands executed are feeded as input for the variables
+
+# EX - date command gives us current date. So, we can extract time taken to execute the commands
+
+start_time=$(date +%s)
+sudo dnf install ansible -y
+end_time=$(date +%s)
+total_time=$(($start_time-$end_time))
+echo "time taken to install Ansible is $total_time"
--- variables.sh
+++ variables.sh
@@ -4,4 +4,22 @@
 
 course=devops
 hours=60
-echo "we found a free $couse learning videos of duration $hours hours"
+echo "we found a free $course learning videos of duration $hours hours"
+
+#Passing variables as arguments
+
+# project=$1
+# env=$2
+# echo "Deployment will be performed on $project in $env environment."
+
+#Read the input into vaibales
+
+# echo "Please enter the PIN number"
+# read pinnumber
+# echo "entered $pinnumber is incorrect"
+
+#Read the input into variables by masking
+
+echo "Please enter valid password for user: ec2-user"
+read -s password
+echo "entered password $password is incorrect"
