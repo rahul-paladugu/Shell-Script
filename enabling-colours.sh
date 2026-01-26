a00580b3907470a5d18498deb1e1895e4b1d735c
rahul paladugu <rahulpaladugu96@gmail.com>
2026-01-27 11:05:24 +1300

    Colours

--- enabling-colours.sh
+++ enabling-colours.sh
@@ -3,10 +3,11 @@
 red="\e[31m"
 green="\e[32m"
 yellow="\e[33m"
+bold="\e[0m"
 reset="\e[0m"
 user=$(id -u)
 if [ "$user" -ne 0 ]; then
-  echo "To install any packages you need root access"
+  echo -e "$bold $yellow To install any packages you need root access $reset"
   echo -e "$red Please run the script using root access $reset"
   exit 1
 fi
