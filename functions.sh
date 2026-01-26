cdf679da8948f4de2b581dbb3e3d1a45f437c351
rahul paladugu <rahulpaladugu96@gmail.com>
2026-01-26 15:08:51 +1300

    functions

---
+++ functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+  echo "Please use root access to install any softwares"
+fi
+
+# Below is the function to re-use block of code when required
+
+Validate() {
+    if [ $1 -ne 0 ]; then
+     echo "Installation of $2 is failed. Please check the logs."
+    else
+     echo "Installation of $2 is successful"
+    fi
+}
+
+dnf install ansible -y
+validate $? ansible
