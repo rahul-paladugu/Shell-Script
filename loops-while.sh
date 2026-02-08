871102b7d88afb1c8d4f6117e82a782285bcfc7a
rahul paladugu <rahulpaladugu96@gmail.com>
2026-02-09 11:26:10 +1300

    While-Loop

---
+++ loops-while.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#While loop keeps running as long as the condition is true.
+
+echo "Please enter a number between 1 to 10"
+read i
+
+while i -gte 10;
+ do 
+   echo "Print current value is $i"
+ done
